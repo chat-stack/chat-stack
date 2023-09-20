78d8ff7ddfe039f35080dd21d66d3eb9eb0f4358
James Zhang <zhjch05@live.com>
2023-09-21 02:02:26 +0800

    typescript and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// credits to https://github.com/twentyhq/twenty/blob/main/server/.eslintrc.js
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -5,7 +7,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'unused-imports'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -15,11 +17,65 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'src/core/@generated/**'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['**../'],
+            message: 'Relative imports are not allowed.',
+          },
+        ],
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'type',
+          'parent',
+          'sibling',
+          'object',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@nestjs/**',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '**/interfaces/**',
+            group: 'type',
+            position: 'before',
+          },
+          {
+            pattern: 'src/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: './*',
+            group: 'sibling',
+            position: 'before',
+          },
+        ],
+        distinctGroup: true,
+        warnOnUnassignedImports: true,
+        pathGroupsExcludedImportTypes: ['@nestjs/**'],
+      },
+    ],
+    'unused-imports/no-unused-imports': 'warn',
   },
 };
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+
+import { AppModule } from 'src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,7 +18,8 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request
+      .default(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
