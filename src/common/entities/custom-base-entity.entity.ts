1844673d98294596c3b2e7bb9f1db2038e25965c
James Zhang <zhjch05@live.com>
2023-09-27 14:08:52 +0800

    swagger fix

--- src/common/entities/custom-base-entity.entity.ts
+++ src/common/entities/custom-base-entity.entity.ts
@@ -1,5 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 import { Entity, OptionalProps, PrimaryKey, Property } from '@mikro-orm/core';
 import { Exclude } from 'class-transformer';
 
@@ -8,17 +6,14 @@ export abstract class CustomBaseEntity<E, K extends keyof E | undefined> {
   [OptionalProps]?: 'createdAt' | 'updatedAt' | K;
 
   @PrimaryKey()
-  @ApiProperty()
   @Exclude()
   id: number;
 
   @Property()
-  @ApiProperty()
   @Exclude()
   createdAt: Date = new Date();
 
   @Property({ onUpdate: () => new Date() })
-  @ApiProperty()
   @Exclude()
   updatedAt: Date = new Date();
 }
