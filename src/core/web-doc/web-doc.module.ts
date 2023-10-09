ed58ff7474276683ed3ef1e7dbb5deed361b5d21
James Zhang <zhjch05@live.com>
2023-10-09 14:59:24 +0800

    fix web doc module

--- src/core/web-doc/web-doc.module.ts
+++ src/core/web-doc/web-doc.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+
 import { LangChainModule } from 'src/core/lang-chain/lang-chain.module';
 
 import { WebDocService } from './web-doc.service';
 import { WebDocController } from './web-doc.controller';
 import { WebDocProcessor } from './web-doc.processor';
 
+import { WebDoc } from './entities/web-doc.entity';
+
 @Module({
   imports: [
+    MikroOrmModule.forFeature([WebDoc]),
     BullModule.registerQueue({
       name: 'webDoc',
     }),
