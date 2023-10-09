5b5a3b114175d2d3cc0e9b3f62559a3b75faaeca
James Zhang <zhjch05@live.com>
2023-10-09 17:48:20 +0800

    rebrand

--- src/main.ts
+++ src/main.ts
@@ -101,8 +101,8 @@ async function bootstrap() {
     }),
   );
   const config = new DocumentBuilder()
-    .setTitle('MetaGPTBot API Docs')
-    .setDescription('MetaGPTBot API Docs')
+    .setTitle('MetadataGPT API Docs')
+    .setDescription('MetadataGPT API Docs')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
